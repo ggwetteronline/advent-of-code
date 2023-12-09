4a7d59d152ca7e773f88ab5bf892f106ae1e7389
gernot.goetz <gernot.goetz@wetteronline.de>
2023-12-09 07:00:47 +0100

    day 09

--- 2023/day09.ts
+++ 2023/day09.ts
@@ -12,14 +12,15 @@ function run(data: string[], part: 'A' | 'B') {
     const tmpRows: number[][] = [];
     tmpRows.push(currentStartRow);
     let deep = 0;
+    // create derived rows
     do {
       tmpRows.push([]);
-      ++deep;
-      for(let i = 1; i < tmpRows[deep-1].length; ++i) {
-        tmpRows[deep].push(tmpRows[deep-1][i] - tmpRows[deep-1][i-1]);
+      for(let i = 1; i < tmpRows[deep].length; ++i) {
+        tmpRows[deep+1].push(tmpRows[deep][i] - tmpRows[deep][i-1]);
       }
-
+      ++deep;
     } while(isRowZero(tmpRows[deep]) == false)
+    // calculate next / previous number
     while( deep > 0) {
       if(part == 'A') {
         tmpRows[deep-1].push(tmpRows[deep-1][tmpRows[deep-1].length-1] + tmpRows[deep][tmpRows[deep].length-1])  
@@ -28,16 +29,17 @@ function run(data: string[], part: 'A' | 'B') {
       }
       --deep;
     }
+    // sum the first / last numbers 
     if(part == 'A') {
-    sum+= tmpRows[0][tmpRows[0].length-1]
+      sum+= tmpRows[0][tmpRows[0].length-1]
     } else {
-    sum+= tmpRows[0][0]
+      sum+= tmpRows[0][0]
     }
   }
-
   return sum;
 }
 
+// check if all values in row are zero
 function isRowZero(arr: number[]) {
   for(let i = 0; i < arr.length; ++i) {
     if(arr[i] != 0) return false;
