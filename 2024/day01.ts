c13d3e6184234ad053e9ef46204b0ec0fd37dc56
gernot.goetz <gernot.goetz@wetteronline.de>
2024-12-01 06:17:55 +0100

    2024 day 1

--- 2024/day01.ts
+++ 2024/day01.ts
@@ -2,31 +2,33 @@ import { lib } from '../lib';
 
 // function logic
 function run(data: string[], part: 'A' | 'B') {
-  const elfCarry: number[] = [];
-  let currElfCarry = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] !== '') {
-      const num = Number.parseInt(data[i]);
-      currElfCarry += num;
-    } else {
-      elfCarry.push(currElfCarry);
-      currElfCarry = 0;
-    }
-  }
-  // after the last elfs inventory is no blank line
-  elfCarry.push(currElfCarry);
+  const nums1: number[] = [];
+  const nums2: number[] = [];
+  data.map((line) => {
+    const [a, b] = line.split('   ').map((a) => Number.parseInt(a));
+    nums1.push(a);
+    nums2.push(b);
+  });
+
   if (part === 'A') {
-    return Math.max(...elfCarry);
+    // sort lists
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    // get distances
+    return nums1.reduce((acc, a, i) => acc + Math.abs(a - nums2[i]), 0);
   } else {
-    elfCarry.sort((a, b) => b - a);
-    return elfCarry[0] + elfCarry[1] + elfCarry[2];
+    return nums1.reduce((acc, a) => {
+      // how often is a in nums2
+      let count = nums2.filter((b) => a == b).length;
+      return acc + a * count;
+    }, 0);
   }
 }
 
 // execute and output
 lib.execute2('2024', 'day01.ts', run, [
-  { name: 'A test', part: 'A', data: 'test', expected: 24000 },
-  { name: 'A prod', part: 'A', data: 'prod', expected: 72602 },
-  { name: 'B test', part: 'B', data: 'test', expected: 45000 },
-  { name: 'B prod', part: 'B', data: 'prod', expected: 207410 },
+  { name: 'A test', part: 'A', data: 'test', expected: 11 },
+  { name: 'A prod', part: 'A', data: 'prod', expected: 2815556 },
+  { name: 'B test', part: 'B', data: 'test', expected: 31 },
+  { name: 'B prod', part: 'B', data: 'prod', expected: 23927637 },
 ]);
